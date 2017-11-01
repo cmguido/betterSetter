c25e7b2b4c1113bc9a422992da12fd69c3ad2c46
cmguido <chris.m.guido@gmail.com>
2017-11-01 18:33:05 -0400

    Add exercises route with exercise model
    Exercise model has been filled with information to pull from db

--- app/routes/exercises.js
+++ app/routes/exercises.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model () {
+    console.log(this.get('store').findAll('exercise'));
+    return this.get('store').findAll('exercise');
+  },
+  actions: {
+  createWorkout (exercise) {
+    console.log(exercise)
+    let newWorkout = this.get('store').createRecord('exercise', exercise);
+    newWorkout.save();
+  }
+}
 });
