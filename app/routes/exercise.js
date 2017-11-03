f10f84ab452da48f1d83bcefc7e5b1c91b16aa40
cmguido <chris.m.guido@gmail.com>
2017-11-03 15:45:20 -0400

    Add fixed date feature
    Does not display days anymore

--- app/routes/exercise.js
+++ app/routes/exercise.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
   export default Ember.Route.extend({
+    flashMessages: Ember.inject.service(),
+
     model (params) {
     return this.get('store').findRecord('exercise', params.exercise_id);
   },
@@ -8,11 +10,20 @@ import Ember from 'ember';
     deleteWorkout() {
       let exercise = this.currentModel
           exercise.destroyRecord()
-            .then(() =>
-              this.transitionTo('exercises'));
+            .then(() => this.transitionTo('exercises'));
+              // .then(() => {
+              //   this.get('flashMessages');
+              //   .success('Workout deleted.');
+              // })
+              // .catch(() => {
+              //   this.get('flashMessages');
+              //   .danger('There was a problem. Please try again.');
+              // });
       },
       editWorkout () {
+        console.log('old date is', this.currentModel._internalModel.__data.date);
         let exercise = this.currentModel
+        console.log(exercise.date)
     exercise.save();
   }
 }
